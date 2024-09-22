710a9f5c6057c10e3c64c1b1b93179aed3a97ea1
Yj <yjgoh28@gmail.com>
2024-09-22 21:18:22 +0800

    Update webcam.js

--- demo/webcam.js
+++ demo/webcam.js
@@ -439,23 +439,3 @@ async function main() {
 
 // Start processing as soon as page is loaded
 window.onload = main;
-
-// Detect face shapes in the video feed
-async function detectFaceShapes(video) {
-    const canvas = faceapi.createCanvasFromMedia(video);
-    document.body.append(canvas);
-
-    const displaySize = { width: video.width, height: video.height };
-    faceapi.matchDimensions(canvas, displaySize);
-
-    setInterval(async () => {
-        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
-                                         .withFaceLandmarks()
-                                         .withFaceExpressions();
-        const resizedDetections = faceapi.resizeResults(detections, displaySize);
-        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-        faceapi.draw.drawDetections(canvas, resizedDetections);
-        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-        faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
-    }, 100);
-}
